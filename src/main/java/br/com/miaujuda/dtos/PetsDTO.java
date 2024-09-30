d10854a3f465fb7dc5f4a1f0004c865f33242d5d
andreluis98 <andreluis0017@gmail.com>
2024-09-30 20:38:07 -0300

    creation the dtos done

---
+++ src/main/java/br/com/miaujuda/dtos/PetsDTO.java
@@ -0,0 +1,18 @@
+package br.com.miaujuda.dtos;
+
+import java.io.Serializable;
+
+public class PetsDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String gender;
+    private String status;
+	private String address;
+	private String observation;
+	private String pet;
+	private String user;
+
+}
