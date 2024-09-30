686729738e7391383a8f8cbc8e690cc944cd26bc
andreluis98 <andreluis0017@gmail.com>
2024-09-30 19:32:57 -0300

    init project backend

---
+++ src/main/java/br/com/miaujuda/MiaujudaApplication.java
@@ -0,0 +1,13 @@
+package br.com.miaujuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaujudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaujudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/miaujuda/MiaujudaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miaujuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaujudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
