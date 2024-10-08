1fc9de45267aa2204b662312f08aea7a72b25333
andreluis98 <andreluis0017@gmail.com>
2024-10-08 20:11:15 -0300

    creation-home-and-menu-done

--- miaujuda-front/src/app/app-routing.module.ts
+++ miaujuda-front/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginScreenComponent } from './login/login-screen/login-screen.component';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginScreenComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
